ba4b899a40004756e6062b67bb6f2388189a9ae8
Alexandra Kolesnikova <alexandra.kolesnikova2013@gmail.com>
2013-04-10 23:06:53 +0400

    clean up of main()

--- testglfw/main.cpp
+++ testglfw/main.cpp
@@ -4,24 +4,18 @@
 using namespace std;
 
 int main(void) {
+    // Initializing Randomizer using current time
     srand(time(0));
 
     cout << "Creating OpenGL launcher...\n";
-
     Launcher * launcher = new Launcher();
 
     cout << "Done, initializing...\n";
-
     if (!launcher->init())
         return -1;
 
     cout << "Done, launching window...\n";
-
     launcher->showWindowWhileNotClosed();
-
-    cout << "Clean up and good bye!\n";
-
-    delete launcher;
 
     return 0;
 }
